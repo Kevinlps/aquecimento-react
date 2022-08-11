fd8392d8ebccb529a6497722d462c1af7e34c0cb
Kevin01~ <kevinlopes3012@gmail.com>
2022-08-10 20:11:03 -0400

    secund commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const root = document.getElementById("root") as HTMLElement;
+const appRoot = ReactDOM.createRoot(root);
+
+const myDiv = (
+  <>
+    <div>
+      <h1>oi</h1>
+      <p>Tudo Bem?</p>
+    </div>
+    <div>
+      <h1>ola</h1>
+      <p>Tudo</p>
+    </div>
+  </>
+);
+
+appRoot.render(myDiv);
